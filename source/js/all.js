f3a9d46fd76964bc152205a91adc6635bd835ecc
Ray <loveyou520babytw@gmail.com>
2019-02-15 08:56:37 +0800

    updata

--- source/js/all.js
+++ source/js/all.js
@@ -3,21 +3,23 @@ Vue.component('cardRepoTheme', {
   template: '#cardRepo'
 })
 
-new Vue({
+const app = new Vue({
   el: '#app',
   data: {
-    //https://gitlab.com/api/v4/users/userid/projects
-    gitlabUrl: 'gitlab.com/api/v4/users/',
+    // https://gitlab.com/api/v4/users/userid/projects
+    gitlabUrl: 'gitlab.com/api/v4/users',
     userName: 'hsiangfeng',
     api: 'projects',
     page: '6',
     authorUrl: 'https://gitlab.com/',
-    data: []
+    data: [],
+    urlAll: ''
   },
   computed: {
-    getrepo() {
+    getrepo () {
       let googleCORS = 'https://script.google.com/macros/s/AKfycbxybXJPnk9oV-VOf5vMAkPKoOmYn-OZ9OXeWO3khPyluXQo6ps/exec?url='
       let str = `${this.gitlabUrl}/${this.userName}/${this.api}?per_page=${this.page}`
+      this.urlAll = str
       window.fetch(googleCORS + str, { method: 'get' })
         .then(response => {
           return response.json()
@@ -32,34 +34,3 @@ new Vue({
     }
   }
 })
-
-const imgBg1 = document.getElementById('bg1-img')
-const imgBg2 = document.getElementById('bg2-img')
-function autoBg1() {
-  if (imgBg1.style.opacity === '1') {
-    imgBg1.style.opacity = '0'
-    imgBg2.style.opacity = '1'
-  } else if (imgBg1.style.display === '0') {
-    imgBg1.style.opacity = '1'
-    imgBg2.style.opacity = '0'
-  }
-  //console.log(window.getComputedStyle(imgBg1).getPropertyPriority('display'))
-  setTimeout(autoBg2, 5000)
-}
-function autoBg2() {
-  if (imgBg2.style.opacity === '0') {
-    imgBg1.style.opacity = '0'
-    imgBg2.style.opacity = '1'
-
-  } else if (imgBg2.style.opacity === '1') {
-    imgBg1.style.opacity = '1'
-    imgBg2.style.opacity = '0'
-  }
-  setTimeout(autoBg1, 5000)
-}
-setTimeout(autoBg1, 5000)
-
-window.onresize = function () {
-  imgBg1.style.height = window.innerHeight + 'px'
-  imgBg2.style.height = window.innerHeight + 'px'
-}
